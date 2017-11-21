34cf349764d9c7e05a5c2593bbc1964ae29bcb38
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-21 09:25:33 -0500

    :0

--- Lab2.2/src/Student.java
+++ Lab2.2/src/Student.java
@@ -4,11 +4,42 @@ public class Student extends Person{
 	private double GPA;
 	private long osis;
 		
-	public Student(int height, String firstName, String lastName, double weight)
+	public Student(int height, String firstName, String familyName, double weight)
 	
 	{
-		
 		super(height, firstName, familyName, weight);
 		this.schedule = schedule;
+		this.GPA = GPA;
+		this.osis = osis;
 	}
+
+	/**
+	 * @return the gPA
+	 */
+	public double getGPA() {
+		return GPA;
+	}
+
+	/**
+	 * @param gPA the gPA to set
+	 */
+	public void setGPA(double gPA) {
+		GPA = gPA;
+	}
+
+	/**
+	 * @return the osis
+	 */
+	public long getOsis() {
+		return osis;
+	}
+
+	/**
+	 * @param osis the osis to set
+	 */
+	public void setOsis(long osis) {
+		this.osis = osis;
+	}
+
+	
 }
